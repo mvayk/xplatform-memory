9cd61d1315be14992af564bdc48c812446e509ea
mvayk <45300955+mvayk@users.noreply.github.com>
2026-03-22 11:19:33 +1100

    blind b at

--- src/bin/twfc.rs
+++ src/bin/twfc.rs
@@ -1,6 +1,6 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-    xplatform_memory::games::tfoc::patch("TWFC_steamless.exe");
+    xplatform_memory::games::twfc::patch("TWFC_steamless.exe");
     Ok(())
 }
